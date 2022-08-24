eeafa770317eab2b86798c7eef4e8e636b4ac08f
Weiqiang <zhuwq0@gmail.com>
2022-08-23 22:14:56 -0700

    fix bug

--- tests/location/inversion3d.jl
+++ tests/location/inversion3d.jl
@@ -6,28 +6,31 @@ using PyPlot
 using Random
 Random.seed!(233)
 
+################## Simulation ##################
 reset_default_graph()
 
 m = 60
 n = 30
 h = 0.1
-
-f = ones(n+1, m+1)
-
 dx = 5
 dy = 5
 
-# srcx = 1:dx:m
-# srcy = 5*ones(Int64, length(1:dx:m))
-srcx = [15, 45]
-srcy = [5, 5]
+vel = ones(n+1, m+1)
 
-recx = collect([20, 30, 40, 50])
-recy = collect([15, 25, 20, 10])
+srcx = [15, 45, 15, 45]
+srcy = [15, 15, 45, 45]
+srcz = [1, 1, 1, 1]
 
-u = PyObject[]
-t = PyObject[]
-magn = []
+# recx = collect(Float64, [20,])
+# recy = collect(Float64, [20,])
+# recz = collect(Float64, [16,])
+recx = collect(Float64, [20, 30, 40, 20, 30, 40, 20, 30, 40,])
+recy = collect(Float64, [20, 20, 20, 30, 30, 30, 40, 40, 40,])
+recz = collect(Float64, [16, 17, 18, 19, 20, 21, 22, 23, 24,])
+
+t_ = PyObject[]
+# u_ = PyObject[]
+# magn_ = []
 
 xs = zeros(length(recx), n+1, m+1)
 ys = zeros(length(recx), n+1, m+1)
@@ -38,62 +41,75 @@ for i = 1:n+1
     end
 end
 
-σ = 1.0 *h
-for (sx, sy) in zip(srcx, srcy)
+σ = 1.0 * h
+for (sx, sy, sz) in zip(srcx, srcy, srcz)
 
-    u_ = eikonal(f,sx,sy,h)
-    push!(u, u_)
-   
-    magn_ = @. 1. /(2π * σ) * exp( - 0.5 * (((xs-recx*h)/σ)^2 + ((ys-recy*h)/σ)^2)) /σ*h^2
-    t_ = sum(u_ .* magn_, dims=(2,3))
-    push!(magn, magn_)
-    push!(t, t_)
+    u = eikonal(vel,1,sz,h)
+    # push!(u_, u)
+
+    R = @. sqrt((recx - sx)^2 + (recy - sy)^2)
+    magn = @. 1. /(2π * σ) * exp( - 0.5 * (((xs-R*h)/σ)^2 + ((ys-recz*h)/σ)^2)) /σ*h^2
+
+    t = sum(u .* magn, dims=(2,3))
+
+    push!(t_, t)
+    # push!(magn_, magn)
 
 end
 
 
 sess = Session()
-tobs = run(sess, t)
-uobs = run(sess, u)
+tobs = run(sess, t_)
+# uobs = run(sess, u)
 
-@show tobs
+# figure()
+# pcolormesh(uobs[1])
+# colorbar()
+# savefig("obs.png")
 
-figure()
-pcolormesh(uobs[1])
-colorbar()
-savefig("obs.png")
-
-figure()
-pcolormesh(magn[1][1,:,:])
-colorbar()
-savefig("magn.png")
+# figure()
+# pcolormesh(magn[1][1,:,:])
+# colorbar()
+# savefig("magn.png")
 
 ## 
-t = PyObject[]
-u = PyObject[]
-magn = PyObject[]
 
-recx = Variable([50.0, 50.0, 50.0, 50.0])
-recy = Variable([5.0, 5.0, 5.0, 5.0])
-recx_ = reshape(recx, [length(recx), 1, 1])
-recy_ = reshape(recy, [length(recy), 1, 1])
+# u = PyObject[]
+# magn = PyObject[]
 
-for (sx, sy) in zip(srcx, srcy)
+################## Inversion ##################
+t_ = PyObject[]
 
-    u_ = eikonal(f,sx,sy,h)
-    push!(u, u_)
+recx_ = Variable(ones(length(recx)) .* 10.0)
+recy_ = Variable(ones(length(recx)) .* 10.0)
+recz_ = Variable(ones(length(recx)) .* 10.0)
+# recx_ = Variable(recx)
+# recy_ = Variable(recy)
+# recz_ = Variable(recz)
+recx = reshape(recx_, [length(recx_), 1, 1])
+recy = reshape(recy_, [length(recy_), 1, 1])
+recz = reshape(recz_, [length(recz_), 1, 1])
 
-    magn_ = 1. /(2π * σ) * exp( - 0.5 * (((xs-recx_*h)/σ)^2 + ((ys-recy_*h)/σ)^2)) /σ*h^2
-    t_ = sum(u_ .* magn_, dims=(2,3))
-    push!(t, t_)
-    push!(magn, magn_)
+for (sx, sy, sz) in zip(srcx, srcy, srcz)
 
+    u = eikonal(vel,1,sz,h)
+    # push!(u, u_)
+
+    R = sqrt((recx - sx)^2 + (recy - sy)^2)
+    magn = 1. /(2π * σ) * exp( - 0.5 * (((xs-R*h)/σ)^2 + ((ys-recz*h)/σ)^2)) /σ*h^2
+    t = sum(u .* magn, dims=(2,3))
+
+    push!(t_, t)
+    # push!(magn, magn_)
 end
 
-loss = sum(sum(abs.(t - tobs)))
+loss = sum(sum(abs.(t_ - tobs)))
 
 init(sess)
-@show run(sess, loss)
+@show "Init loss = ", run(sess, loss)
+
+BFGS!(sess, loss, 200, var_to_bounds=Dict(recx_=>(5.0,m-5.0), recy_=>(5.0,m-5.0), recz_=>(5.0,n-5.0)))
+@show run(sess, [recx_, recy_, recz_])
 
 # error()
 # figure()
@@ -109,15 +125,14 @@ init(sess)
 
 # meshview(sess, pl, loss, F0'[:])
 
-BFGS!(sess, loss, 200, var_to_bounds=Dict(recx=>(5.0,m-5.0), recy=>(5.0,n-5.0)))
 
-magn_ = run(sess, magn)[1]
-for i in 1:length(recx)
-    figure()
-    pcolormesh(magn_[i,:,:])
-    colorbar()
-    savefig("magn_inv_$i.png")
-end
+# magn_ = run(sess, magn)[1]
+# for i in 1:length(recx_)
+#     figure()
+#     pcolormesh(magn_[i,:,:])
+#     colorbar()
+#     savefig("magn_inv_$i.png")
+# end
 
 # figure(figsize=(10, 4))
 # subplot(121)
