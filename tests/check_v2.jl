7afd120c1d992b1ae4563c8d469cd3ddd0812b2b
xialing2003 <xl2524694262@mail.ustc.edu.cn>
2023-09-26 23:28:35 -0700

    checker board test code

--- tests/check_v2.jl
+++ tests/check_v2.jl
@@ -23,7 +23,7 @@ folder = "readin_data/store/new2/2/"
 uobs_p = h5read(folder * "for_P/uobs_p.h5","matrix")
 uobs_s = h5read(folder * "for_S/uobs_s.h5","matrix")
 
-len = 5
+len = 10
 for i = 0:m-1
     for j = 0:n-1
         for k = 0:l-1
@@ -35,15 +35,15 @@ for i = 0:m-1
                 vel0_s[i+1,j+1,k+1] = vel0_s[i+1,j+1,k+1] * 1.2
             else
                 vel0_p[i+1,j+1,k+1] = vel0_p[i+1,j+1,k+1] * 0.8
-                vel0_s[i+1,j+1,k+1] = vel0_s[i+1,j+1,k+1] * 1.2
+                vel0_s[i+1,j+1,k+1] = vel0_s[i+1,j+1,k+1] * 0.8
             end
         end
     end
 end
 fvel_p = 1 ./ vel0_p; fvel_s = 1 ./ vel0_s
 folder = "readin_data/velocity/vel_2/"
-h5write(folder * "vel_check_p.h5","data",fvel_p)
-h5write(folder * "vel_check_s.h5","data",fvel_s)
+h5write(folder * "vel_check_p_10.h5","data",vel0_p)
+h5write(folder * "vel_check_s_10.h5","data",vel0_s)
 
 u_p = PyObject[]; u_s = PyObject[]
 for i = 1:numsta
@@ -73,7 +73,7 @@ for i = 1:numsta
     u0[ixd,iyu,izd] = sqrt((ix-ixd)^2+(iy-iyu)^2+(iz-izd)^2)*slowness_sd*h
     u0[ixd,iyd,izu] = sqrt((ix-ixd)^2+(iy-iyd)^2+(iz-izu)^2)*slowness_su*h
     u0[ixd,iyd,izd] = sqrt((ix-ixd)^2+(iy-iyd)^2+(iz-izd)^2)*slowness_sd*h
-    push!(u_s,eikonal3d(u0,fvel_p,h,m,n,l,1e-3,false))
+    push!(u_s,eikonal3d(u0,fvel_s,h,m,n,l,1e-3,false))
 end
 
 sess = Session(); init(sess)
@@ -149,5 +149,5 @@ for i = 1:numeve
 end
 
 folder = "readin_data/store/new2/2/"
-h5write(folder * "for_P/ucheck_p.h5","matrix",ucheck_p)
-h5write(folder * "for_S/ucheck_s.h5","matrix",ucheck_s)
+h5write(folder * "for_P/ucheck_p_10.h5","matrix",ucheck_p)
+h5write(folder * "for_S/ucheck_s_10.h5","matrix",ucheck_s)
