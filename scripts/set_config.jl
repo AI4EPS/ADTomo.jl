348c4cb7bd3e32407d0262e90c7d2813992b26aa
xialing2003 <xl2524694262@mail.ustc.edu.cn>
2023-10-13 22:48:15 -0700

    codes in scripts

--- scripts/set_config.jl
+++ scripts/set_config.jl
@@ -1,6 +1,6 @@
 using JSON
 
-region = "BayArea"
+region = "BayArea/"
 config = Dict(
     "sta_eve" => Dict(
         "region" => region,
@@ -20,12 +20,22 @@ config = Dict(
         "sta_eps" => 1.414,
         "eve_ratio" => 25
     ),
+    "gene_obs" => Dict(
+        "prange" => 1.5,
+        "srange" => 3,
+        "p_requirement" => 0.8,
+        "s_requirement" => 0.6,
+        "p_times" => 1.042,
+        "s_times" => 1.083,
+        "bins_p" => 6000,
+        "bins_s" => 5000
+    ),
 )
 
 json_str = JSON.json(config, 3)  
 json_str = json_str * "\n"
 
-folder = "../local"*region*"readin_data/"
+folder = "../local/"*region*"readin_data/"
 if !isdir(folder) mkdir(folder) end 
 json_file = folder * "config.json"
 
@@ -54,6 +64,16 @@ config = Dict(
         "eve_eps" => 1.732,
         "sta_eps" => 1.414,
         "eve_ratio" => 25
+    ),
+    "gene_obs" => Dict(
+        "prange" => 1.5,
+        "srange" => 3,
+        "p_requirement" => 0.8,
+        "s_requirement" => 0.6,
+        "p_times" => 1.027,
+        "s_times" => 1.064,
+        "bins_p" => 1000,
+        "bins_s" => 1000
     ),
 )
 
